7d56325484ef845339f8d573069d2a223515ac6f
sahilkhatri <sahilk.khatri@gmail.com>
2024-03-13 17:56:40 +0000

    version updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'holboxai',         # How you named your package folder (MyLib)
   packages = ['holboxai'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'HolboxAI package',   # Give a short description about your library
   author = 'Sahil Khatri',                   # Type in your name
