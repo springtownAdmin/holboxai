73f5877c5eda36d59c70ddcc971da53f0e10dcf7
Rohith KA <rohith.ka555@gmail.com>
2024-03-14 20:00:18 +0530

    Syncing Repository

--- holboxai/__init__.py
+++ holboxai/__init__.py
@@ -1,7 +1,4 @@
 from holboxai.file1 import firstclass
 from holboxai.file2 import secondclass
-<<<<<<< HEAD
-from holboxai.Rohith import Instructor
-=======
 from holboxai.file3 import sort
->>>>>>> 9fa2d2c8674e37d7a93f5b25ac8fc310645d5932
+from holboxai.Rohith import Instructor
