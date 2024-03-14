b9e73d58586a913d13fa19e8a2c7c3bc50b61a55
NairVishnuVinod <nairvishnu26523@gmail.com>
2024-03-14 16:34:06 +0530

    Rag file with witcher directory

--- holboxai/vishnuRagFile.py
+++ holboxai/vishnuRagFile.py
@@ -1,5 +1,22 @@
-import diffusers
+import getpass
+import os
+os.environ["OPENAI_API_KEY"] = getpass.getpass()
 
-class firstclass:
+
+import os.path
+from llama_index.core import (
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    StorageContext,
+    load_index_from_storage,
+)
+
+class RagLlama:
     def rag():
-        print("here we do rag")
+        documents = SimpleDirectoryReader("The Witcher documents").load_data()
+        index = VectorStoreIndex.from_documents(documents)
+
+        query_engine_witcher = index.as_query_engine()
+
+        response_witcher = query_engine_witcher.query("what is amazon bedrock")
+        return response_witcher
