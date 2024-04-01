14d58ae5720091c070738b64fd7435cd54b81972
Pavan3avn <pavankumaravn3388@gmail.com>
2024-04-01 11:14:06 +0530

    file4deletd

--- holboxai/file4.py
+++
@@ -1,9 +0,0 @@
-class twosum:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        dict  = {}
-        for i, num in enumerate(nums):
-            index = target - num
-            if index in dict:
-                return [dict[index],i]
-            dict[num] = i
-        return []
