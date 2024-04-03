e4fc457516bc6211ee73a6bf0c50c5446149baf7
EC2 Default User <ec2-user@ip-172-16-13-247.us-west-2.compute.internal>
2024-04-03 05:42:02 +0000

    updated init

--- holboxai/__init__.py
+++ holboxai/__init__.py
@@ -5,3 +5,4 @@ from holboxai.s3_reader import S3DocReader
 from holboxai.docs_query import DocsQuery
 from holboxai.relevant_docs import RelevantDocs
 from holboxai.csv_query import CsvQuery
+from holboxai.sentiment_analysis import SentimentAnalysis
