a46a3b6cbfdcc3d98fbe10760d30e2a9c7967692
sahilkhatri <sahilk.khatri@gmail.com>
2024-04-03 11:38:12 +0000

    package updated with t2i and sentiment with requirements

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 setup(
   name = 'holboxai',         # How you named your package folder (MyLib)
   packages = ['holboxai'],   # Chose the same as "name"
-  version = '0.6',      # Start with a small number and increase it with every change you make
+  version = '0.7',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'HolboxAI package',   # Give a short description about your library
   long_description = 'Welcome to HolboxAI, a comprehensive AI package designed to enhance your data processing and creative capabilities. HolboxAI offers a range of functionalities including text-to-image generation, running textual queries on documents stored in your S3 bucket, and generating insights from natural language queries.',
   author = 'Sahil Khatri',                   # Type in your name
   author_email = 'sahil.khatri@holbox.ai',      # Type in your E-Mail
   url = 'https://github.com/springtownAdmin/holboxai',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/springtownAdmin/holboxai/archive/refs/tags/v_07.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/springtownAdmin/holboxai/archive/refs/tags/v_08.tar.gz',    # I explain this later on
   keywords = ['GenAI', 'Custom', 'holbox'],   # Keywords that define your package best
   install_requires=[          
           'diffusers',
@@ -18,7 +18,7 @@ setup(
           'langchain',
           'langchain_community',
           'unstructured[pdf]',
-          'unstructured[docs]',
+          'unstructured[docx]',
           'pandasai',
       ],
   classifiers=[
