020ad9f4833b71f337ac04b58e46867d9072db21
Sahil Khatri <36559039+sahilkhatri@users.noreply.github.com>
2024-04-03 13:50:36 +0530

    Delete holboxai/file2.py

--- holboxai/file2.py
+++
@@ -1,9 +0,0 @@
-import flask
-import numpy as np
-
-class secondclass:
-    def demo():
-        a = [1,2,3]
-        a = np.array(a)
-        print("here we show our live demo")
-        print("sample numpy array: ", a)
