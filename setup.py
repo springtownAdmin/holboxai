3ef2b402c1a6be99842a18584c9a94230f2faa5c
sahilkhatri <sahilk.khatri@gmail.com>
2024-04-11 13:29:20 +0000

    resolved requirement issue

--- setup.py
+++ setup.py
@@ -18,14 +18,14 @@ setup(
   download_url = 'https://github.com/springtownAdmin/holboxai/archive/refs/tags/v_13.tar.gz',    # I explain this later on
   keywords = ['GenAI', 'Custom', 'holbox'],   # Keywords that define your package best
   install_requires=[
-      boto3,
-      langchain,
-      langchain_community,
-      unstructured[pdf],
-      unstructured[docx],
-      pandasai,
-      langchainhub,
-      faiss-cpu,
+      'boto3',
+      'langchain',
+      'langchain_community',
+      'unstructured[pdf]',
+      'unstructured[docx]',
+      'pandasai',
+      'langchainhub',
+      'faiss-cpu',
   ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
